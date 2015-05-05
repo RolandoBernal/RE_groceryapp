f0f6b052c389bebbd1c1707381714341b1d8b4d7
Rolando Bernal <jrolandobernal@gmail.com>
2015-05-04 20:31:41 -0500

    Added budget to DB

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
     username: {type: String, required: true, default: ''},
-    password: {type: String, required: true, default: ''}
+    password: {type: String, required: true, default: ''},
+    budget: {type: Number, required: true, default: 0}
 });
 
 var User = mongoose.model('User', userSchema);
