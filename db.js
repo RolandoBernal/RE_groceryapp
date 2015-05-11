7b14ea2980e26bf62c64e0b3e4b96140bd0c8b19
Rolando Bernal <jrolandobernal@gmail.com>
2015-05-11 18:25:12 -0500

    Heroku DBjs

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/grocery');
+// mongoose.connect('mongodb://localhost/grocery');
+mongoose.connect(process.env.DB);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
